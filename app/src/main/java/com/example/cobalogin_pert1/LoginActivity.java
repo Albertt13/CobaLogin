c20e9bbe324730d0ca7e26b23ef8fe4887b11a9f
Albertt13 <dhikaalbertus@mhs.mdp.ac.id>
2023-03-01 08:55:45 +0700

    kodwaodaw

--- app/src/main/java/com/example/cobalogin_pert1/LoginActivity.java
+++ app/src/main/java/com/example/cobalogin_pert1/LoginActivity.java
@@ -2,13 +2,58 @@ package com.example.cobalogin_pert1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    KendaliLogin KL = new KendaliLogin();   //Pemanggilan class KendaliLogin
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username;
+                String password;
+
+                username = etUsername.getText().toString();
+                password = etPassword.getText().toString();
+
+//                username = "AlbertusDwiAndhika";
+//                password = "akuganteng";
+
+                if (username.trim().equals("")) {
+                    etUsername.setError("Username Tidak Boleh Kosong");
+                }
+                else if (password.trim().equals("")) {
+                    etPassword.setError("Password Tidak Boleh Kosong");
+                }
+                else {
+                    if (username.equals("albert") && password.equals("ggwahyu")) {
+                        KL.setPref(LoginActivity.this, MainActivity.keySPusername, username);
+
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(LoginActivity.this, "Username atau Password Salah!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cobalogin_pert1/MainActivity.java
+++ app/src/main/java/com/example/cobalogin_pert1/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,12 +18,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String sesi;
 
         if (KL.isLogin(MainActivity.this, keySPusername) ==  true) {
             setContentView(R.layout.activity_main);
 
+            sesi = KL.getPref(MainActivity.this, keySPusername);
             tvSesi = findViewById(R.id.tv_sesi);
+            tvSesi.setText(sesi);
             btnLogout = findViewById(R.id.btn_logout);
+
+            btnLogout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    KL.setPref(MainActivity.this, keySPusername, null);
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                }
+            });
+
         }
         else {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
